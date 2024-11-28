72a6c5b5a2eabc9a1fe2086c3f0a2a687168133e
anandavel <32697457+anandavel@users.noreply.github.com>
2024-11-28 17:41:13 +0530

    ne

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
-// App.js
 
-import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Header from './components/Header.js';
-import Footer from './components/Footer.js';
 
-function App() {
-	return (
-	<>
-	<Header />
-		<main>
-			<Container className="text-center my-5">
-			<Row>
-				<Col md={6} className="bg-primary text-white p-4">
-					<h2>Column 1</h2>
-					<p>This is a visually appealing layout using the grid system approach.</p>
-					<Button variant="light">Click me</Button>
-				</Col>
-				<Col md={6} className="bg-secondary text-white p-4">
-					<h2>Column 2</h2>
-					<p>Responsive and stunning design to enhance user experience.</p>
-					<Button variant="light">Explore</Button>
-				</Col>
-			</Row>
-		</Container>
-		</main>
-		<Footer />
-	</>
-		
-	);
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Blogs from "./pages/Blog";
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path="blogs" element={<Blogs />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+              
+
---
+++ src/pages/Blog.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+function Footer() {
+    return (
+        <>
+         <Container>
+            <Row>
+                <Col md={12}>
+                <p>Blog</p>
+                </Col>
+            </Row>
+        </Container>
+        </>       
+    );
+  }
+
+export default Footer;

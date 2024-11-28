1df36f811b0a6c4c31bc7afb10acb7ada93e05cd
anandavel <32697457+anandavel@users.noreply.github.com>
2024-11-28 17:42:46 +0530

    df

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Blogs from "./pages/Blog";
+import Blog from "./pages/Blog";
 
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="blogs" element={<Blogs />} />
+          <Route path="blog" element={<Blog />} />
         </Route>
       </Routes>
     </BrowserRouter>
